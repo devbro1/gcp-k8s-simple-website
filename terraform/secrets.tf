3c1bcc455d484db6821894e96052da03cf718d39
farzad meow khalafi <farzadk@gmail.com>
2024-02-09 16:41:34 -0500

    used secret for password

---
+++ terraform/secrets.tf
@@ -0,0 +1,36 @@
+data "google_client_config" "default" {
+}
+
+provider "kubernetes" {
+  host             = "https://${google_container_cluster.primary.endpoint}"
+  token            = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(
+    google_container_cluster.primary.master_auth[0].cluster_ca_certificate
+  )
+}
+
+data "kubernetes_namespace" "default" {
+  metadata {
+    name = "default"
+  }
+
+  depends_on = [google_container_node_pool.primary_nodes]
+}
+
+resource "random_password" "db_password" {
+  length = 16
+}
+
+resource "kubernetes_secret" "db_secrets" {
+  metadata {
+    name      = "db-credentials"
+    namespace = data.kubernetes_namespace.default.metadata[0].name
+  }
+
+  data = {
+    username = "dbuser1"
+    password = random_password.db_password.result
+    url = "postgresql+psycopg2://dbuser1:${urlencode(random_password.db_password.result)}@postgresql/tododb"
+    url_replica = "postgresql+psycopg2://dbuser1:${urlencode(random_password.db_password.result)}@postgresql_replica/tododb"
+  }
+}
